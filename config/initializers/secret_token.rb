20417da4ed3c5b8e8b4c699e9e0b594152768193
Joey Lapetina <joeylapetina@gmail.com>
2013-12-04 21:59:53 -0600

    setting up a new program to help me get an xbox one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XboxGetOne::Application.config.secret_token = '597c3bb975cb7adc666872f29cb7ac6f409c25e2b541f003b631218aa9c37c32bf004e47ecea34872f753546e93e5c55580fc8a0a105b04360a6b9c297470cd8'
